d15fb0d4a7a429da61dde4b94f401c2c9c347492
Vuthuyhang <vuthuyhang102004@gmail.com>
2025-11-23 23:32:39 +0700

    update homepage

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to My Homepage</h1>
+      <p>Đây là trang chủ React của bạn.</p>
+
+      <button className="btn">Go to Login</button>
+    </div>
+  );
+}
+
+export default Home;
